3d774dcce149f97fadab58596ea4c298e9ce0903
richard-ma <richard.ma.19850509@gmail.com>
2024-11-28 18:28:07 +0800

    update world map

--- src/lifegame/core.py
+++ src/lifegame/core.py
@@ -30,8 +30,62 @@ if __name__ == "__main__":
     width, height = 9, 9
     world_map = [[0 for i in range(width)] for j in range(height)]
 
-    new_world_map = [[0 for i in range(width)] for j in range(height)]
+    # init world
 
+    
+    new_world_map = [[0 for i in range(width)] for j in range(height)]
+    # update world
+    for i in range(height):
+        for j in range(width):
+            sum_array = list()
+            if i == 0:
+                sum_array.append(world_map[i+1][j]) 
+                if j == 0:
+                    sum_array.append(world_map[i][j+1])
+                    sum_array.append(world_map[i+1][j+1])
+                elif j == width - 1:
+                    sum_array.append(world_map[i][j-1])
+                    sum_array.append(world_map[i+1][j-1])
+                else:
+                    sum_array.append(world_map[i][j+1])
+                    sum_array.append(world_map[i+1][j+1])
+                    sum_array.append(world_map[i][j-1])
+                    sum_array.append(world_map[i+1][j-1])
+            elif i == height - 1:
+                sum_array.append(world_map[i-1][j])
+                if j == 0:
+                    sum_array.append(world_map[i][j+1])
+                    sum_array.append(world_map[i-1][j+1])
+                elif j == width - 1:
+                    sum_array.append(world_map[i][j-1])
+                    sum_array.append(world_map[i-1][j-1])
+                else:
+                    sum_array.append(world_map[i][j+1])
+                    sum_array.append(world_map[i-1][j+1])
+                    sum_array.append(world_map[i][j-1])
+                    sum_array.append(world_map[i-1][j-1])
+            else:
+                sum_array.append(world_map[i-1][j-1])
+                sum_array.append(world_map[i-1][j])
+                sum_array.append(world_map[i-1][j+1])
+                sum_array.append(world_map[i][j+1])
+                sum_array.append(world_map[i+1][j+1])
+                sum_array.append(world_map[i+1][j]) 
+                sum_array.append(world_map[i+1][j-1])
+                sum_array.append(world_map[i][j-1])
+            s = sum(sum_array)
+            is_alive = world_map[i][j]
+            if is_alive == 0 and s == 3:
+                new_is_alive = 1
+            else: # self._is_alive = 1
+                if s < 2 or s > 3:
+                    new_is_alive = 0
+                else: # around sum is 2 or 3
+                    new_is_alive = 1
+            new_world_map[i][j] = new_is_alive
+                    
+
+    # print world map
     for i in range(height):
         for j in range(width):
             print(world_map[i][j], end='')
