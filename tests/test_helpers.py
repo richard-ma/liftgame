114147f8478bd931c5a91372a0efa6a652e5da48
richard-ma <richard.ma.19850509@gmail.com>
2024-12-10 15:02:39 +0800

    helper tests

---
+++ tests/test_helpers.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestHelpers(unittest.TestCase):
+    def setUp(self):
+        return super().setUp()
+    
+    def tearDown(self):
+        return super().tearDown()
+
+    def returnTrue(self):
+        return True
+    
+    def test_map_printer(self):
+        pass
