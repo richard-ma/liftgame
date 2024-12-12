8f4148952f2ebe592b28581ab254fd677d1e67c0
richard-ma <richard.ma.19850509@gmail.com>
2024-12-12 14:51:00 +0800

    add Lifegame

--- src/lifegame/__init__.py
+++ src/lifegame/__init__.py
@@ -4,7 +4,13 @@ from .world import World
 from .worldhistory import WorldHistory
 
 
+class Lifegame:
+    def __init__(self):
+        self._world_history = WorldHistory()
+
+
 all = [
     World, 
     WorldHistory,
+    Lifegame,
     ]
