f89660cb60f7184d87e52ca574b6896b411aa8dc
richard-ma <richard.ma.19850509@gmail.com>
2024-12-12 09:40:07 +0800

    remove MapPrinter

--- src/lifegame/__init__.py
+++ src/lifegame/__init__.py
@@ -1,12 +1,10 @@
 #-*- coding: utf-8 -*-
 
-from .helpers import MapPrinter
 from .world import World
 # from .worldhistory import WorldHistory
 
 
 all = [
-    MapPrinter, 
     World, 
     # WorldHistory,
     ]
