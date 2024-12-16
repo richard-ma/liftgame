39f849cc22790c6b6f4b928b4b9f27b37a3dccd5
richard-ma <richard.ma.19850509@gmail.com>
2024-12-16 09:38:38 +0800

    move world_history to setUp

--- src/tests/test_world_history.py
+++ src/tests/test_world_history.py
@@ -7,24 +7,19 @@ class TestWorldHistory(unittest.TestCase):
         self.first_history = World([1, 1])
         self.second_history = World([1, 2])
         self.last_history = World([1, 3])
+
+        self.world_history = WorldHistory()
+        self.world_history.append_history(self.first_history)
+        self.world_history.append_history(self.second_history)
+        self.world_history.append_history(self.last_history)
         return super().setUp()
 
     def tearDown(self):
         return super().tearDown()
     
     def test_append_history(self):
-        world_history = WorldHistory()
-        world_history.append_history(self.first_history)
-        self.assertEqual(1, world_history.len())
-        world_history.append_history(self.second_history)
-        self.assertEqual(2, world_history.len())
-        world_history.append_history(self.last_history)
-        self.assertEqual(3, world_history.len())
+        self.assertEqual(3, self.world_history.len())
 
     def test_save_history(self):
-        world_history = WorldHistory()
-        world_history.append_history(self.first_history)
-        world_history.append_history(self.last_history)
-
-        world_history.save("history_test.json")
-        world_history.load("history_test.json")
+        self.world_history.save("history_test.json")
+        self.world_history.load("history_test.json")
